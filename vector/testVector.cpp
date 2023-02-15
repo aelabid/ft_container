c19e750939fc8f5de96306e407f8faf6a223a0cb
Asmaa Elabid <aelabid@e1r5p2.1337.ma>
2023-02-15 03:42:42 +0100

    start implementing avl tree

--- vector/testVector.cpp
+++ vector/testVector.cpp
@@ -1,26 +1,59 @@
-#include"../includes/vector.hpp"
-#include<iostream>
-#include<vector>
-#include<unistd.h>
+// #include"../includes/vector.hpp"
+// #include<iostream>
+// #include<vector>
+// #include<unistd.h>
 
-  // vector<int> myvector (10);   // 10 zero-initialized ints
+//   // vector<int> myvector (10);   // 10 zero-initialized ints
 
-  // assign some values:
+//   // assign some values:
+// int main()
+// {
+//   vector<int> foo (3,100);   // three ints with a value of 100
+//   vector<int> bar (5,200);   // five ints with a value of 200
+
+//   foo.swap(bar);
+
+//   std::cout << "foo contains:";
+//   for (unsigned i=0; i<foo.size(); i++)
+//     std::cout << ' ' << foo[i];
+//   std::cout << '\n';
+
+//   std::cout << "bar contains:";
+//   for (unsigned i=0; i<bar.size(); i++)
+//     std::cout << ' ' << bar[i];
+//   std::cout << '\n';
+
+// }
+
+// C++ program to illustrate the
+// iterators in vector
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 int main()
 {
-  vector<int> foo (3,100);   // three ints with a value of 100
-  vector<int> bar (5,200);   // five ints with a value of 200
+	std::vector<int> g1;
 
-  foo.swap(bar);
+	for (int i = 1; i <= 5; i++)
+		g1.push_back(i);
 
-  std::cout << "foo contains:";
-  for (unsigned i=0; i<foo.size(); i++)
-    std::cout << ' ' << foo[i];
-  std::cout << '\n';
+	cout << "Output of begin and end: ";
+	for (vector<int>::iterator i = g1.begin(); i != g1.end(); ++i)
+		cout << *i << " ";
+    
+	// cout << "\nOutput of cbegin and cend: ";
+	// for (vector<int>::iterator i = g1.cbegin(); i != g1.cend(); ++i)
+	// 	cout << *i << " ";
 
-  std::cout << "bar contains:";
-  for (unsigned i=0; i<bar.size(); i++)
-    std::cout << ' ' << bar[i];
-  std::cout << '\n';
+	// cout << "\nOutput of rbegin and rend: ";
+	// for (vector<int>::iterator ir = g1.rbegin(); ir != g1.rend(); ++ir)
+	// 	cout << *ir << " ";
 
+	// cout << "\nOutput of crbegin and crend : ";
+	// for (vector<int>::iterator ir = g1.crbegin(); ir != g1.crend(); ++ir)
+	// 	cout << *ir << " ";
+
+	return 0;
 }
